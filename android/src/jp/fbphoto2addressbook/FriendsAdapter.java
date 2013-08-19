1470be8da0be71be775028b8dd513cd694bd4db1
daisy1754 <d-part@hotmail.co.jp>
2013-08-19 22:26:50 +0900

    add profile picture loading functionality using volley

--- android/src/jp/fbphoto2addressbook/FriendsAdapter.java
+++ android/src/jp/fbphoto2addressbook/FriendsAdapter.java
@@ -1,12 +1,18 @@
 package jp.fbphoto2addressbook;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 
 import java.util.*;
 
@@ -16,11 +22,15 @@ import java.util.*;
 public class FriendsAdapter extends ArrayAdapter<FbFriend> {
     private LayoutInflater inflater;
     private Set<FbFriend> selectedFriends;
+    private RequestQueue queue;
+    private ImageLoader imageLoader;
 
     public FriendsAdapter(Context context) {
         super(context, R.layout.list_item_friend, new LinkedList<FbFriend>());
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         selectedFriends = new HashSet<FbFriend>();
+        queue = Volley.newRequestQueue(getContext());
+        imageLoader = new ImageLoader(queue, new BitmapCache());
     }
 
     @Override
@@ -42,7 +52,40 @@ public class FriendsAdapter extends ArrayAdapter<FbFriend> {
                 }
             }
         });
-// TODO:        ((ImageView) view.findViewById(R.id.friend_picture)).setImageURI(item.getPhotoUri());
+
+        ImageLoader.ImageListener listener = ImageLoader.getImageListener(
+                ((ImageView) view.findViewById(R.id.friend_picture)),
+                R.drawable.ic_contact_picture,
+                android.R.drawable.ic_delete);
+        imageLoader.get(friend.getPictureUrl(), listener);
+
         return view;
+    }
+
+    // copied from http://dev.classmethod.jp/smartphone/android/android-tips-51-volley/
+    private static class BitmapCache implements ImageLoader.ImageCache {
+
+        private LruCache<String, Bitmap> mCache;
+
+        public BitmapCache() {
+            int maxSize = 10 * 1024 * 1024;
+            mCache = new LruCache<String, Bitmap>(maxSize) {
+                @Override
+                protected int sizeOf(String key, Bitmap value) {
+                    return value.getRowBytes() * value.getHeight();
+                }
+            };
+        }
+
+        @Override
+        public Bitmap getBitmap(String url) {
+            return mCache.get(url);
+        }
+
+        @Override
+        public void putBitmap(String url, Bitmap bitmap) {
+            mCache.put(url, bitmap);
+        }
+
     }
 }
